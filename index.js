f10d4ce2990bd4dce1435e06006f80c9b057c96a
Jônatas Thielke <jonatas1927@gmail.com>
2020-04-22 15:41:51 -0300

    added bcrypt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 var CryptoJSsha1 = require("crypto-js/sha1");
 const commandLineArgs = require('command-line-args')
+const bcrypt = require('bcrypt');
 
 
 const optionDefinitions = [
@@ -21,6 +22,14 @@ if (fs.existsSync('users.json')) {
 
 var usuario = { usuario: options.user, alg: options.alg };
 
+const escreveJson = function () {
+    fs.writeFile('users.json', JSON.stringify(users), (error) => {
+        if (error)
+            console.log('deu erro meu parceiro')
+    })
+}
+
+
 switch (options.alg) {
     case 'sha1':
         const salt = new Date().toISOString()
@@ -28,13 +37,26 @@ switch (options.alg) {
         usuario.hash = CryptoJSsha1(options.pass, salt).toString();
         console.log(usuario)
         users.usuarios.push(usuario)
+        escreveJson()
+        break;
+    case 'bcrypt':
+        const saltRounds = 15;
+        for (let i = 0; i < 1; i++) {
+            let usuario = { usuario: options.user, alg: options.alg };
+
+            let tInicial = new Date()
+            let salt = bcrypt.genSaltSync(saltRounds);
+            let hash = bcrypt.hashSync(options.pass, salt);
+            usuario.salt = salt;
+            usuario.hash = hash
+            usuario.rodadas = saltRounds
+            let tFinal = new Date()
+            usuario.tempo = (tFinal - tInicial) / 1000
+            users.usuarios.push(usuario)
+        }
+        escreveJson()
         break;
     default:
         break;
 }
-
-fs.writeFile('users.json', JSON.stringify(users), (error) => {
-    if (error)
-        console.log('deu erro meu parceiro')
-})
 
